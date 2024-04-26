53d049d16542f6f61c0e2df5de6df297a46d0364
Fischer Moseley <42497969+fischermoseley@users.noreply.github.com>
2024-04-26 08:12:10 -0700

    run Surfer with Webview API

--- src/extension.ts
+++ src/extension.ts
@@ -20,6 +20,48 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+	// start
+	context.subscriptions.push(
+		vscode.commands.registerCommand('surfer.start', async () => {
+
+		  	// Create and show panel
+		  	const panel = vscode.window.createWebviewPanel(
+				'surferPanel',
+				'Surfer Waveform Viewer',
+				vscode.ViewColumn.One,
+				{
+					// Enable scripts in the webview
+					enableScripts: true,
+
+					// Only allow the webview to access resources in our extension's media directory
+					localResourceRoots: [vscode.Uri.joinPath(context.extensionUri, 'surfer')]
+				}
+			);
+
+			// Get path to resource on disk
+			const indexPath = vscode.Uri.joinPath(context.extensionUri, 'surfer', 'index.html');
+			const contents = await vscode.workspace.fs.readFile(indexPath)
+
+			// Replace local paths with paths derived from WebView URIs
+			let html = new TextDecoder().decode(contents);
+			html = replaceAll(html, "./manifest.json", pathToURIString(context, panel, "surfer", "manifest.json"));
+			html = replaceAll(html, "./surfer_bg.wasm", pathToURIString(context, panel, "surfer", "surfer_bg.wasm"));
+			html = replaceAll(html, "./surfer.js", pathToURIString(context, panel, "surfer", "surfer.js"));
+			html = replaceAll(html, "./sw.js", pathToURIString(context, panel, "surfer", "sw.js"));
+
+			panel.webview.html = html;
+		})
+	  );
+}
+
+function replaceAll(input: string, search: string, replacement: string): string {
+    return input.replace(new RegExp(search, 'g'), replacement);
+}
+
+function pathToURIString(context:vscode.ExtensionContext, panel: vscode.WebviewPanel, ...pathSegments: string[]): string {
+	let path = vscode.Uri.joinPath(context.extensionUri, ...pathSegments)
+	return panel.webview.asWebviewUri(path).toString()
 }
 
 // This method is called when your extension is deactivated
